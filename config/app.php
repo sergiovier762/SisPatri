0185b0e2324a0422e4698f4b8a3fc1046c0b286d
sergiovier762 <sergiovier762@gmail.com>
2024-11-13 14:07:19 -0300

    att excel

--- config/app.php
+++ config/app.php
@@ -123,4 +123,10 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'aliases' => [
+        // Outros aliases...
+        'Redirect' => Illuminate\Support\Facades\Redirect::class,
+        // Outros aliases...
+    ],
+
 ];
